66410322afcdea2330a3822c87234c144e6da207
Amir HR <133565009+amir2robo@users.noreply.github.com>
2025-08-06 20:30:30 +0330

    Update taskify.py

--- 1_task_tracker/taskify.py
+++ 1_task_tracker/taskify.py
@@ -10,7 +10,7 @@
 # delete 2
 """
 
-# color: colorma
+# color: colorama
 # table: tabulate
 
 # Start APP
@@ -19,6 +19,7 @@ from colorama import Fore, Back, Style
 import json
 from datetime import datetime as dt
 import os
+from tabulate import tabulate
 
 
 # Good Thing TODO
@@ -51,6 +52,19 @@ def where_is_it(filename=FILENAME):
     with open(filename, "r") as file:
         tasks = json.load(file)
     return len(tasks)
+
+
+def read_all_tasks(filename=FILENAME):
+    """Function To read all json file tasks"""
+    try:
+        if os.path.exists(filename):
+            with open(filename, "r") as file:
+                return json.load(file)
+        else:
+            return None
+    except:
+        with open(filename, "w") as file:
+            file.write("[]")
 
 
 def save_changes(task, filename=FILENAME):
@@ -87,14 +101,32 @@ def manage_arg(arguments):
     # command
     arg = arguments[1].lower()
 
+    # Add Task
     if arg in ["add", "a"]:
-        add_task(arguments[2])
+        os.system("clear")
+        # just add new task
+        last_added = add_task(arguments[2])
+        # just show last added
+        temp_list = []
+        temp_list.append(last_added)
+
+        list_task(temp_list)
+        del temp_list
+
     elif arg in ["del", "delete", "remove"]:
         delete_task()
     elif arg in ["update", "edit"]:
         update_task()
+
+    # Show Table
     elif arg in ["show", "list", "table"]:
-        list_task()
+        os.system("clear")
+        list_task(read_all_tasks())
+
+    # clear Tasks
+    elif arg in ["clear", "clean"]:
+        clear_list()
+
     else:
         print(Fore.YELLOW + Style.BRIGHT + "Invalid Command.")
 
@@ -119,6 +151,8 @@ def add_task(text):
 
     save_changes(new_task)
 
+    return new_task
+
     # show list : TODO
 
 
@@ -130,8 +164,43 @@ def update_task():
     pass
 
 
-def list_task():
-    pass
+def list_task(tasks):
+    """Funciton To Show List in Table Tasks"""
+    table = []
+
+    if not tasks:
+        print(Fore.RED + Style.BRIGHT + "\nNo Avalable Tasks !\n")
+        return
+
+    # loop inject tasks to task and finaly inject to table(rows)
+    for task in tasks:
+        table.append(
+            [
+                task["id"],
+                task["title"],
+                task["status"],
+                task["created_at"],
+                task["updated_at"],
+            ]
+        )
+
+    # combine rows and column
+    final_table = tabulate(
+        table, headers=["ID", "Title", "Status", "Created At", "Updated At"]
+    )
+
+    # just show table
+    print(Fore.GREEN + Style.BRIGHT + f"\n\n{final_table}\n\n")
+
+
+def clear_list(filename=FILENAME):
+    os.system("clear")
+
+    # claering
+    with open(filename, "w") as file:
+        file.write("[]")
+
+    print(Fore.YELLOW + Style.BRIGHT + "All Tasks cleared.")
 
 
 if __name__ == "__main__":
